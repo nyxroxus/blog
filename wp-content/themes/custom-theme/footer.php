61d67f460d671b44c5940aeac75970d18a3ba00c
Nyx Roxus <jovana@arkham-enterprises.com>
2017-10-20 13:32:57 +0200

    First commit: Created a new theme from scratch

---
+++ wp-content/themes/custom-theme/footer.php
@@ -0,0 +1,17 @@
+</div><!-- /.container -->
+
+    <footer class="">
+      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+   <script src="https://use.fontawesome.com/65486b663f.js"></script>
+<?php wp_footer(); ?>
+  </body>
+</html>
---
+++ wp-content/themes/custom-theme/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo get_bloginfo( 'name' ) ?></title>
+    <!-- Notice this -->
+    <link href="<?php echo get_bloginfo('template_directory') ?>/library/css/main.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+      <?php  wp_head(); ?>
+  </head>
+<body>
+<div class="w-90 center">
+    <div class="w-30-l w-70 fl fl-l" style="">
+        <div class="">
+            <h1 class="custom-padding-menu f3"><?php echo get_bloginfo( 'name' ) ?></h1>
+        </div>
+    </div>
+
+    <div class="w-70-l fr-l dn dib-l" style="">
+        <h1 class="f4-l f5 tr-l"><a class="pulse" href="#"><?php wp_list_pages( '&title_li=' ); ?></a></h1> 
+    </div>
+    <div class="w-30 fr mt2 dib dn-l">
+        <i class="pointer fa fa-bars fa-3x fr custom-padding-menu"></i>
+        <!-- Add navigation menu functionality -->
+    </div>
+</div>
